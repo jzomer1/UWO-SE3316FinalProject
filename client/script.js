2bfd6a855548d3aa8aed08781ee1b60bd2c094bc
JustinZomer <jzomer@uwo.ca>
2023-11-08 19:17:33 -0500

    front-end functionality getting publishers(A3 8.2)

--- client/script.js
+++ client/script.js
@@ -1,39 +1,36 @@
-const superheroInfo = document.getElementById('superheroInfo');
-const superheroPowers = document.getElementById('superheroPowers')
+const searchResults = document.getElementById('searchResults');
 
 function addBoxShadow(data) {
     data.classList.add('box-shadow-effect');
 }
 
 function getHeroInfo() {
-    addBoxShadow(superheroInfo);
+    addBoxShadow(searchResults);
 
     const ID = document.getElementById('heroId').value;
 
     fetch(`/superheroes/${ID}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(`: ID ${ID} not found`);
+                throw new Error(`ID ${ID} not found`);
             }
             return response.json();
         })
         .then(data => {
-            const superheroInfo = document.getElementById('superheroInfo');
             const content = `<b>ID:</b> ${data.id}<br><b>Name:</b> ${data.name}<br><b>Gender:</b> ${data.Gender}
             <br><b>Eye colour:</b> ${data['Eye color']}<br><b>Race:</b> ${data.Race}<br><b>Hair colour:</b> ${data['Hair color']}
             <br><b>Height:</b> ${data.Height}<br><b>Publisher:</b> ${data.Publisher}<br><b>Skin colour:</b> ${data['Skin color']}
             <br><b>Alignment:</b> ${data.Alignment}<br><b>Weight:</b> ${data.Weight}`;
-            superheroInfo.innerHTML = content;
+            searchResults.innerHTML = content;
         })
         .catch(error => {
             console.error(`Error ${error}`);
-            const superheroInfo = document.getElementById('superheroInfo');
-            superheroInfo.innerHTML = `Error ${error.message}`;
+            searchResults.innerHTML = `Error: ${error.message}`;
         });
 }
 
 function getHeroPowers() {
-    addBoxShadow(superheroPowers);
+    addBoxShadow(searchResults);
 
     const name = document.getElementById('heroId2').value;
 
@@ -45,20 +42,41 @@ function getHeroPowers() {
             return response.json();
         })
         .then(data => {
-            const superheroPowers = document.getElementById('superheroPowers');
-            // const powers = data.powers;
-            
             if (data.length > 0) {
                 const content = `<b>Powers for ${name}:</b> ${data.join(', ')}`;
-                superheroPowers.innerHTML = content;
+                searchResults.innerHTML = content;
             }
             else {
-                superheroPowers.innerHTML = 'No powers found';
+                searchResults.innerHTML = 'No powers found';
             }
         })
         .catch(error => {
             console.error(`Error: ${error}`);
-            const superheroInfo = document.getElementById('superheroPowers');
-            superheroInfo.innerHTML = `Error: ${error.message}`;
+            searchResults.innerHTML = `Error: ${error.message}`;
+        });
+}
+
+function getPublisherList() {
+    addBoxShadow(searchResults);
+
+    fetch(`/publishers`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Publishers not found`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 0) {
+                const content = `<b>Publishers are:</b> ${data.join(', ')}`;
+                searchResults.innerHTML = content;
+            }
+            else {
+                searchResults.innerHTML = 'Publishers not found';
+            }
+        })
+        .catch(error => {
+            console.error(`Error: ${error}`);
+            searchResults.innerHTML = `Error: ${error.message}`;
         });
 }
