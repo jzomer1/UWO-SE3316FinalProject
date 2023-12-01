3334b215c81af95726be54500d9d39b53326062f
JustinZomer <jzomer@uwo.ca>
2023-12-01 01:11:30 -0500

    feat: basic navbar and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+// import Navbar from './components/navbar';
+import Navbar from './components/navbar';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Hello</h1>
       <Routes>
+        <Route path='/' element={ <Home /> }/>
+        <Route path='/login' element={ <Login /> }/>
+        <Route path='/register' element={ <Register /> }/>
       </Routes>
     </>
   );
