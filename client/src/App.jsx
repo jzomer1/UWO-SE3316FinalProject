9454ca89118369eef59c82a07ef2caade1658935
JustinZomer <jzomer@uwo.ca>
2023-11-30 22:37:01 -0500

    feat: install and set up router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
+      <h1>Hello</h1>
+      <Routes>
+      </Routes>
     </>
   );
 }
