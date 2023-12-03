237f41dc971b184091efaee35710fe105597b7cc
JustinZomer <jzomer@uwo.ca>
2023-12-02 22:15:13 -0500

    feat: error handling for SearchHeroes

--- client/src/components/SearchHeroes.jsx
+++ client/src/components/SearchHeroes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function SearchHeroes() {    
+export default function SearchHeroes() {
     const [error, setError] = useState(null);
     const [results, setResults] = useState(null);
     const [expandedResults, setExpandedResults] = useState([]);
@@ -15,82 +15,78 @@ export default function SearchHeroes() {
         try {
             const response = await fetch(`/match?field=${field}&pattern=${pattern}&n=${n}`);
             console.log(response);
-      
+
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
-        
+
                 if (Array.isArray(data)) {
-                  setResults(data);
-                  setError(null);
-                  setExpandedResults([]);
+                    setResults(data);
+                    setError(null);
+                    setExpandedResults([]);
                 } else {
-                  setResults([]);
-                  setError('Invalid data format');
+                    setResults([]);
+                    setError('Invalid data format');
                 }
               } else {
-                setResults([]);
-                setError('Invalid response status: ' + response.status);
+                    setResults([]);
+                    setError('Invalid response status: ' + response.status);
               }
             } catch (error) {
-              console.error(`Error searching heroes: ${error}`);
-              setResults([]);
-              setError('Error: ' + error.message);
+                console.error(`Error searching heroes: ${error}`);
+                setResults([]);
+                setError('Error: ' + error.message);
             }
     };
     const expand = () => {
-        // const mainSearchResults = document.getElementById('mainSearchResults');
-        // mainSearchResults.innerHTML = '';
         let expandedResultsArray = [];
     
         if (Array.isArray(results)) {
-          expandedResultsArray = results.map((superhero) => {
-            const attributes = [];
-            let heroName;
-    
-            for (const property in superhero) {
-              if (superhero.hasOwnProperty(property)) {
-                const propertyName = (property === 'id') ? 'ID' : property.charAt(0).toUpperCase() + property.slice(1);
-                attributes.push(
-                  <div key={property}><b>{propertyName}:</b> {superhero[property]}</div>
-                );
-    
-                if (propertyName === 'Name') {
-                  heroName = superhero[property];
+            expandedResultsArray = results.map((superhero) => {
+                const attributes = [];
+                let heroName;
+        
+                for (const property in superhero) {
+                if (superhero.hasOwnProperty(property)) {
+                    const propertyName = (property === 'id') ? 'ID' : property.charAt(0).toUpperCase() + property.slice(1);
+                    attributes.push(
+                        <div key={property}><b>{propertyName}:</b> {superhero[property]}</div>
+                    );
+        
+                    if (propertyName === 'Name') {
+                        heroName = superhero[property];
+                    }
                 }
-              }
-            }
-    
-            if (heroName) {
-              const duckButton = (
-                <button key={`duckButton-${superhero.id}`} onClick={() => duckSearch(heroName)}>
-                  Search on DDG
-                </button>
-              );
-              attributes.push(duckButton);
-            }
-    
-            return (
-              <div key={superhero.id} className="hero-block box-shadow-effect">
-                {attributes}
-              </div>
-            );
-          });
-    
-          setExpandedResults(expandedResultsArray);
+                }
+        
+                if (heroName) {
+                    const duckButton = (
+                        <button key={`duckButton-${superhero.id}`} onClick={() => duckSearch(heroName)}>Search on DDG</button>
+                    );
+                    attributes.push(duckButton);
+                }
+        
+                return (
+                    <div key={superhero.id} className="hero-block box-shadow-effect">
+                        {attributes}
+                    </div>
+                );
+            });
+
+            setExpandedResults(expandedResultsArray);
         } else {
           alert('Please search first');
         }
-      };
+    };
     const duckSearch = (name) => {
         const searchUrl = `https://duckduckgo.com/?q=${encodeURIComponent(name)}`;
         // open search results in new tab
         window.open(searchUrl, '_blank');
     };
 
-  return (
+    return (
     <div>
-      <p>Select Field: </p>
+        <p>Select Field: </p>
         <select id="field">
             <option value="name">Name</option>
             <option value="Race">Race</option>
@@ -111,7 +107,7 @@ export default function SearchHeroes() {
         <button onClick={expand}>Expand Results</button>
         <div id="mainSearchResults">
             {expandedResults.length === 0 ? (
-            // Display initial search results
+            // initial results
             results?.map((superhero) => (
                 <div key={superhero.id} className="hero-block box-shadow-effect">
                     <div><b>Name:</b> {superhero.name}</div>
@@ -120,10 +116,11 @@ export default function SearchHeroes() {
                 </div>
             ))
             ) : (
-            // Display expanded results
+            // expanded results
             expandedResults
             )}
         </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
-  );
+    );
 }
